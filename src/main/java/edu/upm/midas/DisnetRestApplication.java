250ff4ea5b681ee2a687a42e97f069f06ced156d
Gerardo Lagunes García <g.lagunes@alumnos.upm.es>
2019-04-26 15:30:50 +0200

    Implementación de CORSFilter

--- src/main/java/edu/upm/midas/DisnetRestApplication.java
+++ src/main/java/edu/upm/midas/DisnetRestApplication.java
@@ -1,12 +1,15 @@
 package edu.upm.midas;
 
+import edu.upm.midas.configuration.CORSFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
 
 @EnableFeignClients
 @EnableAutoConfiguration
@@ -18,5 +21,15 @@ public class DisnetRestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DisnetRestApplication.class, args);
+	}
+
+	@Bean
+	public FilterRegistrationBean corsFilterRegistration() {
+		FilterRegistrationBean registrationBean =
+				new FilterRegistrationBean(new CORSFilter());
+		registrationBean.setName("CORS Filter");
+		registrationBean.addUrlPatterns("/*");
+		registrationBean.setOrder(1);
+		return registrationBean;
 	}
 }
